94228944b73176d90102fc2417ec182c62a1559c
nirmalhelicore <nirmalhelicore@gmail.com>
2022-11-02 14:31:28 +0530

    Changes

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -66,7 +66,7 @@ const Dashboard = () => {
         </div>
       </div>
       <div className="row gx-2 my-4">
-        <div className="col-9 col-lg-9 total-response-graph-box p-3">
+        <div className="col-9 total-response-graph-box p-3">
           <div className="row">
             <div className="col-8 col-lg-8">
               <div className="d-flex justify-content-between">
@@ -118,7 +118,7 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className="col-3 col-lg-3 total-response-box p-3">
+        <div className="col-3 total-response-box p-3">
           <div className="d-flex">
             <div
               style={{ backgroundColor: "rgba(255, 255, 255, 0.2)" }}
